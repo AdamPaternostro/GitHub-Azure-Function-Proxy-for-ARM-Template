c4c53e15d01f1a228193dea8e66be84c5ccd5c16
Adam Paternostro <adampaternostro@Adams-MBP.home>
2018-12-19 17:10:08 -0500

    Updates

---
+++ Azure-Function.cs
@@ -0,0 +1,59 @@
+#r "Newtonsoft.Json"
+
+using System.Net;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+// See: https://github.com/AdamPaternostro/GitHub-Azure-Function-Proxy-for-ARM-Template
+
+public static async Task<IActionResult> Run(Microsoft.AspNetCore.Http.HttpRequest req, ILogger log)
+{
+    try
+    {
+        log.LogInformation("BEGIN: GitHub Proxy Request");
+        
+        string personalAcccessToken = "{REMOVED}";
+        string responseBody = "";
+
+        string location = req.Query["location"];
+        log.LogInformation("location: " + location);
+
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(
+                    System.Text.ASCIIEncoding.ASCII.GetBytes(
+                        string.Format("{0}:{1}", "", personalAcccessToken))));
+
+            using (HttpResponseMessage response = client.GetAsync(location).Result)
+            {
+                response.EnsureSuccessStatusCode();
+                responseBody = await response.Content.ReadAsStringAsync();
+                // log.LogInformation(responseBody);
+
+                return responseBody != null
+                    ? (ActionResult)new OkObjectResult(responseBody)
+                    : new BadRequestObjectResult("No file returned from GitHub");
+
+
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        log.LogInformation(ex.ToString());
+        return new BadRequestObjectResult("An Error occurred in the Azure Function");
+    }
+    finally
+    {
+        log.LogInformation("END: GitHub Proxy Request");
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # GitHub-Azure-Function-Proxy-for-ARM-Template
 Allows you to use your private GitHub repo for Azure Linked ARM templates.
+
+## Create the Azure Function
+1. Create an Azure Function of type .NET named GitHubProxy
+2. Paste the Azure-Function.cs into a new function
+3. Login into GitHub
+   - Click on your name (top right)
+   - Select Settings
+   - Go to Developer Settings
+   - Go to Personal Access Token
+   - Click Generate New Token
+   - Give it name
+   - Select repo scope access
+4. Paste your Personal Access Token into the Azure Function 
+   - string personalAcccessToken = "{REMOVED}"; 
+
+## To call the Azuze Function
+- Press the "Get function URL" in the Azure Portal
+- e.g. https://{removed}.azurewebsites.net/api/{removed}?code=<<removed>>
+- Add the query string &location={raw url in GitHub}
+- Sample: https://{removed}.azurewebsites.net/api/{removed}?code=<<removed>>&location=https://raw.githubusercontent.com/AdamPaternostro/GitHub-Azure-Function-Proxy-for-ARM-Template/master/README.md
+- You can test this in a browser window
+
+## To Run via command line (Linux)
+- You need to change the URL in the azuredeploy.json
+```
+# Login
+az login
+
+# Select Subscription
+az account set -s REPLACE_ME
+
+# Script parameters
+resourceGroup="Azure-Function-Proxy"
+location="eastus"
+today=`date +%Y-%m-%d-%H-%M-%S`
+deploymentName="MyDeployment-$today"
+
+# Create resource group
+az group create \
+  --name        $resourceGroup \
+  --location    $location
+
+# Deploy the ARM template
+az group deployment create \
+  --name                 $deploymentName \
+  --resource-group       $resourceGroup \
+  --template-file        azuredeploy.json
+
+# Clean up resource group
+az group delete --name $resourceGroup
+```
